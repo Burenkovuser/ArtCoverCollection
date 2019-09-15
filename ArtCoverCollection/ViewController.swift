ff48a202a49a1fd31afd4f403cc4c5bbfc380b57
Burenkovuser <nb2207@yandex.ru>
2019-09-15 15:53:09 +0700

    Initial Commit

---
+++ ArtCoverCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtCoverCollection
+//
+//  Created by Vasilii on 15/09/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
